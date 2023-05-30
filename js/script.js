a465b9936a8249c56c5b425dc1c74c7e01a5deec
Exceed-Digital <exceed.webmaster@gmail.com>
2023-05-30 13:58:27 +0800

    push

--- js/script.js
+++ js/script.js
@@ -40,6 +40,12 @@ $(document).ready(function() {
     };
 });
 
+function showAlert(){
+  if($("#myAlert").find("div#myAlert2").length==0){
+    $("#myAlert").append("<div class='alert alert-success alert-dismissable' id='myAlert2'> <button type='button' class='close' data-dismiss='alert'  aria-hidden='true'>&times;</button> Success! message sent successfully.</div>");
+  }
+  $("#myAlert").css("display", "");
+}
   
 // For printing images
     function ImagetoPrint(source)
